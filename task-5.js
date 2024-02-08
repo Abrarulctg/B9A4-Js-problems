059d11169ee383eda0683f8020c484966de5e46b
Abrarulctg <jubairctg99@gmail.com>
2024-02-08 14:35:06 +0600

    Finished task-5 :

---
+++ task-5.js
@@ -0,0 +1,28 @@
+function monthlySavings(array, livingCost) {
+    let totalEarning = array;
+    if (Array.isArray(totalEarning) === true && typeof livingCost === 'number') {
+        let sumOfEarning = 0;
+        for (let singleEarning of totalEarning) {
+            if (singleEarning >= 3000) {
+                const tax = singleEarning * (20 / 100);
+                const remainingArr = singleEarning - tax;
+                singleEarning = remainingArr;
+            }
+            sumOfEarning = sumOfEarning + singleEarning;
+        }
+        const totalSavings = sumOfEarning - livingCost;
+        if (totalSavings < 0) {
+            return "Earn More";
+        }
+        else {
+            return totalSavings;
+        }
+    }
+    else {
+        return "invalid input";
+    }
+}
+
+const res = monthlySavings([900, 2700, 3400], 500);
+
+console.log(res)
